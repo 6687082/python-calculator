6a303defeceaa35ec4cc016f14ff61d74ef0dc57
6687082 <noppawut.ler@student.mahidol.edu>
2024-11-07 07:27:29 +0000

    Added test case

--- calculator.py
+++ calculator.py
@@ -3,23 +3,40 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        return a - b
 
     def multiply(self, a, b):
         result = 0
-        for i in range(b+1):
-            result = self.add(result, a)
-        return result
+        if a == 0 or b == 0:
+            return result
+        elif b < 0 and a < 0:
+            a = -(a)
+            b = -(b)
+            for i in range(b):
+                result = self.add(result,a)
+            return result
+        elif b < 0:
+            for i in range(a):
+                result = self.add(result,b)
+            return result
+        else:
+            for i in range(b):
+                result = self.add(result, a)
+            return result
 
     def divide(self, a, b):
         result = 0
-        while a > b:
+        while a >= b:
             a = self.subtract(a, b)
             result += 1
+        if a < 0:
+            while a <= b and a != 0:
+                a = self.add(a, b)
+                result -= 1
         return result
     
     def modulo(self, a, b):
-        while a <= b:
+        while b <= a:
             a = a-b
         return a
 
@@ -27,8 +44,8 @@ class Calculator:
 if __name__ == "__main__":
     calc = Calculator()
     print("This is a simple calculator class!")
-    print("Example: addition: ", calc.add(1, 2))
-    print("Example: subtraction: ", calc.subtract(4, 2))
-    print("Example: multiplication: ", calc.multiply(2, 3))
-    print("Example: division: ", calc.divide(10, 2))
-    print("Example: modulo: ", calc.modulo(10, 3))
+    print("Example: addition: ", calc.add(20, 2))
+    print("Example: subtraction: ", calc.subtract(10, 10))
+    print("Example: multiplication: ", calc.multiply(-2,-6))
+    print("Example: division: ", calc.divide(-20, 2))
+    print("Example: modulo: ", calc.modulo(10,3))
--- test_calculator.py
+++ test_calculator.py
@@ -9,7 +9,32 @@ class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
 
+
     # Add the following test methods to the TestCalculator class:
+    def test_add2(self):
+        self.assertEqual(self.calc.add(20, -2), 18)
+    # min
+    def test_min(self):
+        self.assertEqual(self.calc.subtract(3, 2), 1)  
+    def test_min2(self):
+        self.assertEqual(self.calc.subtract(10, 20), -10)
+    # mul
+    def test_mul(self):
+        self.assertEqual(self.calc.multiply(-6, -2), 12)
+    def test_mul2(self):
+        self.assertEqual(self.calc.multiply(10, 2), 20)  
+    # div
+    def test_div(self):
+        self.assertEqual(self.calc.divide(20, 10), 2)
+    def test_div2(self):
+        self.assertEqual(self.calc.divide(-20, 10), -2)
+    # mod
+    def test_mod(self):
+        self.assertEqual(self.calc.modulo(10, 3), 1)  
+    def test_mod2(self):
+        self.assertEqual(self.calc.modulo(3, 10), 3)
+
+      
 
 if __name__ == '__main__':
     unittest.main()
